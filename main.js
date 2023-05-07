6b86a2cb4a15281e64d9d554d02d4424391da19d
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-05-07 03:48:27 -0400

    implements question extraction logic

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 fetch("http://localhost:3000/questions")
 .then(resp => resp.json())
-.then(questions => console.log(questions[0]))
+.then(questions => {
+    const container = document.querySelector("ul#optionList");
+    const ques = document.querySelector("#question");
+    ques.textContent = questions[0].question;
+    questions[0].options.forEach(element => {
+        const opt = document.createElement("ul")
+        opt.textContent = element;
+        container.appendChild(opt);
+    });
+})
